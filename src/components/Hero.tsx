56f90d4da60fcd38a00552c11c357b800c5006b7
martinryberglaude <martin.ryberg.laude@gmail.com>
2021-05-15 15:16:19 +0200

    redesign home hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import styles from './Hero.module.scss'
 import { Starfield } from './Starfield'
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkerAlt, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Hero() {
 
@@ -10,12 +10,26 @@ export default function Hero() {
         <div className={styles.wrapper}>
             <div className={styles.homeBackground} />
             <div className={styles.center}>
-                <h1 className={styles.title}>Martin Ryberg Laude</h1>
-                <h2 className={styles.desc}>Android & Web Developer</h2>
-                <div className={styles.locationWrapper}>
-                    <FontAwesomeIcon className={styles.locationIcon} icon={faMapMarkerAlt} />
-                    <p className={styles.locationText}>Stockholm, Sweden</p>
+                <h1>Martin <span>R. Laude</span></h1>
+                <div className={styles.subHeading}>
+                    <div className={styles.innerWrapper + " " + styles.leftWrapper}>
+                        <h3>Creative Web Developer</h3>
+                    </div>
+                    <div className={styles.innerWrapper + " " + styles.ellipsisWrapper}>
+                        <div className={styles.ellipsis}>
+                            <div/>
+                            <div/>
+                            <div/>
+                        </div>
+                    </div>
+                    <div className={styles.innerWrapper + " " + styles.rightWrapper}>
+                        <div className={styles.locationWrapper}>
+                            <FontAwesomeIcon className={styles.locationIcon} icon={faMapMarkerAlt} />
+                            <h3>Stockholm, Sweden</h3>
+                        </div>
+                    </div>
                 </div>
+                <FontAwesomeIcon className={styles.downIcon} icon={faChevronDown} onClick={() => document.getElementById("projects")?.scrollIntoView({ behavior: 'smooth', block: 'start' })} />
             </div>
             <div className={styles.starsWrapper}>
                 <Starfield
