530effcbcf3a33da307967e08472448d94237633
martinryberglaude <martin.ryberg.laude@gmail.com>
2021-05-17 12:48:54 +0200

    improve portfolio item design

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -20,35 +20,41 @@ export default function Portfolio() {
                             title="Solsken"
                             paragraph="Weather made easy. This is a weather application for Scandinavia and Finland using api's from SMHI and YR.no (MET Norway) to deliver accurate and detailed forecast."
                             tags={["Java", "MVP", "Android"]}
-                            img={imgSolsken}/>
+                            img={imgSolsken}
+                            color={"#6B6AABDD"}/>
                     </Link>
                     <Link to="/solskenweb" className={styles.wide}>
                         <PortfolioItem 
                             title="Solsken PWA"
                             paragraph="The progressive web app version of Solsken, now with support for the whole world."
                             tags={["React", "Sass", "Typescript", "PWA"]}
-                            img={imgSolskenWeb}/>
+                            img={imgSolskenWeb}
+                            color={"#377B6CDD"}/>
                     </Link>
                     <Link to="/avocado">
                         <PortfolioItem
                             title="Avocado"
                             paragraph="Productivity PWA application built in React with the latest technologies at the time."
                             tags={["React", "Sass", "Typescript", "PWA"]}
-                            img={imgAvocado}/>
+                            img={imgAvocado}
+                            color={"#F9E9B0DD"}
+                            blackText={true}/>
                     </Link>
                     <Link to="/quantumsnake">
                         <PortfolioItem 
                             title="Quantum Snake"
                             paragraph="A C# .NET Framework console snake game with an A* powered enemy snake made in an 80's outrun theme."
                             tags={[".NET", "A* pathfinding", "Game Engine"]}
-                            img={imgQuantum}/>
+                            img={imgQuantum}
+                            color={"#00000060"}/>
                     </Link>
                     <Link to="/pool" className={styles.wide}>
                         <PortfolioItem 
                             title="Pahlén Poolkalkylator"
                             paragraph="A web-based pool chemistry calculator built for Pahlén AB."
                             tags={["React", "Typescript", "CSS"]}
-                            img={imgPahlen}/>
+                            img={imgPahlen}
+                            color={"#0068B2DD"}/>
                     </Link>
                 </div>
         </div>
--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -6,15 +6,21 @@ type Props = {
     paragraph: string
     tags: string[]
     img: string
+    color: string
+    blackText?: boolean
 }
 export default function PortfolioItem(props: Props) {
+
+    function getTextColor(): string {
+        return props.blackText ? "#000000" : "#ffffff"
+    }
 
     return (
         <div className={styles.wrapper}>
             <img src={props.img} alt="thumbnail"/>
-            <div className={styles.content}>
-                <h3 className={styles.title}>{props.title}</h3>
-                <p className={styles.paragraph}>{props.paragraph}</p>
+            <div className={styles.content} style={{background: props.color}}>
+                <h3 className={styles.title} style={{color: getTextColor()}}>{props.title}</h3>
+                <p className={styles.paragraph} style={{color: getTextColor()}}>{props.paragraph}</p>
                 <div className={styles.tags}>
                     {props.tags.map((tag, index) => {
                         return (<p key={index} className={styles.tag}>{tag}</p>)
