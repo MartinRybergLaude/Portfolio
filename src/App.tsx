d0e7f7b63aff01c6337111849eb25bb2cd833e0c
Martin R-L <martin.ryberg.laude@gmail.com>
2021-05-23 18:08:44 +0200

    add codesplitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import './App.scss'
 import {
   BrowserRouter as Router,
@@ -8,39 +8,43 @@ import {
 import Home from './Pages/Home'
 import Footer from 'components/Footer'
 import NotFound from './Pages/NotFound'
-import CaseSolskenWeb from 'Pages/CaseSolskenWeb'
-import CaseSolsken from 'Pages/CaseSolsken'
-import CaseQuantum from 'Pages/CaseQuantum'
+import Loading from 'components/Loading'
+
+const CaseSolskenWeb = React.lazy(() => import("Pages/CaseSolskenWeb"))
+const CaseSolsken = React.lazy(() => import("Pages/CaseSolsken"))
+const CaseQuantum = React.lazy(() => import("Pages/CaseQuantum"))
 
 function App() {
   return (
     <>
       <div id="content">
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Home/>
-            </Route>
-            <Route exact path="/solsken">
-              <CaseSolsken/>
-            </Route>
-            <Route exact path="/solskenweb">
-              <CaseSolskenWeb/>
-            </Route>
-            <Route exact path="/avocado">
-              <NotFound isUnfinished={true}/>
-            </Route>
-            <Route exact path="/quantumsnake">
-              <CaseQuantum/>
-            </Route>
-            <Route exact path="/pool">
-              <NotFound isUnfinished={true}/>
-            </Route>
-            <Route>
-              <NotFound/>
-            </Route>
-          </Switch>
-        </Router>
+          <Router>
+            <Suspense fallback={Loading}>
+              <Switch>
+                <Route exact path="/">
+                  <Home/>
+                </Route>
+                <Route exact path="/solsken">
+                  <CaseSolsken/>
+                </Route>
+                <Route exact path="/solskenweb">
+                  <CaseSolskenWeb/>
+                </Route>
+                <Route exact path="/avocado">
+                  <NotFound isUnfinished={true}/>
+                </Route>
+                <Route exact path="/quantumsnake">
+                  <CaseQuantum/>
+                </Route>
+                <Route exact path="/pool">
+                  <NotFound isUnfinished={true}/>
+                </Route>
+                <Route>
+                  <NotFound/>
+                </Route>
+              </Switch> 
+            </Suspense>
+          </Router>
       </div>
       <Footer />
     </>
